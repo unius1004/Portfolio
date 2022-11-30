3195a42e94fca4016be900c6b5300d6785fbe6fb
unius1004 <unius1004@gmail.com>
2022-11-30 16:18:25 +0900

    절대모멘텀 전략 백테스트 추가

--- strategy/strategy.py
+++ strategy/strategy.py
@@ -1,6 +1,6 @@
 import bt
 
-def Buy_n_Hold(data, name="Buy and Hold") :
+def Buy_n_Hold(data, name="Buy and Hold"):
     s = bt.Strategy(name, [bt.algos.RunOnce(),
                            bt.algos.PrintInfo('{name}:{now}. Value:{_value:0.0f}, Price:{_price:0.4f}'),
                            bt.algos.PrintDate(),                           
@@ -11,7 +11,7 @@ def Buy_n_Hold(data, name="Buy and Hold") :
     return bt.Backtest(s, data, initial_capital=100000000.0)
 
 
-def Equally_Weighted(data, name="Equally Weighted") :
+def Equally_Weighted(data, name="Equally Weighted"):
     s = bt.Strategy(name, [bt.algos.RunMonthly(run_on_first_date=False, run_on_end_of_period=True, run_on_last_date=False),
                            #bt.algos.PrintInfo('{name}:{now}. Value:{_value:0.0f}, Price:{_price:0.4f}'),
                            #bt.algos.PrintDate(),
@@ -21,7 +21,7 @@ def Equally_Weighted(data, name="Equally Weighted") :
                            bt.algos.Rebalance()])
     return bt.Backtest(s, data, initial_capital=100000000.0)
 
-def Absolute_Momentum(data, weights, name="Absolute Momentum") :
+def Absolute_Momentum(data, weights, name="Absolute Momentum"):
     s = bt.Strategy(name, [bt.algos.RunMonthly(run_on_first_date=False, run_on_end_of_period=True, run_on_last_date=False),
                            #bt.algos.RunAfterDate(start),
                            #bt.algos.PrintInfo('{name}:{now}. Value:{_value:0.0f}, Price:{_price:0.4f}'),
