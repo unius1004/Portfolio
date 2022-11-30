23fe6efcf342d6f4f2b2ebc09df1ad09f33da7ee
unius1004 <unius1004@gmail.com>
2022-11-30 15:58:12 +0900

    코스피 지수 절대모멘텀백테스트 추가

---
+++ setting/common.py
@@ -0,0 +1,25 @@
+import FinanceDataReader as fdr
+import numpy as np
+import pandas as pd
+
+def getHistory(tickers, names) :
+    data = pd.DataFrame()
+    for i in range(0, len(tickers)) :
+        raw = fdr.DataReader(tickers[i])
+        data[names[i]] = raw['Close']
+    return data.dropna()
+
+def getWeightByAbsoluteMomentum(assets, months=12):
+    momentum = assets / assets.shift(months) - 1
+    print('momentum \n', momentum.head(30))
+
+    weights = pd.DataFrame(np.zeros((len(assets),2)), columns=assets.columns)
+    weights['Date'] = assets.index
+    weights = weights.set_index('Date')
+
+    for i in range(0, len(weights)):
+        if momentum[assets.columns[0]].iloc[i] >= 0:
+            weights[assets.columns[0]].iloc[i] = 1
+        else:
+            weights[assets.columns[1]].iloc[i] = 1
+    return weights
--- strategy/strategy.py
+++ strategy/strategy.py
@@ -1,14 +1,17 @@
 import bt
 
-def Buy_n_Hold(data, name='Buy and Hold'):
+def Buy_n_Hold(data, name="Buy and Hold"):
     s = bt.Strategy(name, [bt.algos.RunOnce(),
+                           bt.algos.PrintInfo('{name}:{now}. Value:{_value:0.0f}, Price:{_price:0.4f}'),
+                           bt.algos.PrintDate(),                           
                            bt.algos.SelectAll(),
                            bt.algos.WeighEqually(),
+                           bt.algos.PrintTempData(),
                            bt.algos.Rebalance()])
     return bt.Backtest(s, data, initial_capital=100000000.0)
 
 
-def Equally_Weighted(data, name='Equally Weighted'):
+def Equally_Weighted(data, name="Equally Weighted"):
     s = bt.Strategy(name, [bt.algos.RunMonthly(run_on_first_date=False, run_on_end_of_period=True, run_on_last_date=False),
                            #bt.algos.PrintInfo('{name}:{now}. Value:{_value:0.0f}, Price:{_price:0.4f}'),
                            #bt.algos.PrintDate(),
@@ -17,3 +20,15 @@ def Equally_Weighted(data, name='Equally Weighted'):
                            #bt.algos.PrintTempData(),
                            bt.algos.Rebalance()])
     return bt.Backtest(s, data, initial_capital=100000000.0)
+
+def Absolute_Momentum(data, weights, name="Absolute Momentum"):
+    s = bt.Strategy(name, [bt.algos.RunMonthly(run_on_first_date=False, run_on_end_of_period=True, run_on_last_date=False),
+                           #bt.algos.RunAfterDate(start),
+                           #bt.algos.PrintInfo('{name}:{now}. Value:{_value:0.0f}, Price:{_price:0.4f}'),
+                           #bt.algos.PrintDate(),
+                           bt.algos.SelectAll(),
+                           bt.algos.WeighTarget(weights),
+                           #bt.algos.PrintTempData(),
+                           bt.algos.Rebalance()])
+    return bt.Backtest(s, data, initial_capital=100000000.0)
+    
