d0662cb63bd3b17f2ef7e2168fd50c9eb556799c
unius1004 <unius1004@gmail.com>
2022-12-02 07:11:49 +0900

    update

--- setting/common.py
+++ setting/common.py
@@ -49,19 +49,22 @@ def getWeightByAvgMomentumScore(assets):
 # 기준자산(dualticker[0])과 투자자산(ticker)의 순위(rank)내 자산의 n(=month)개월 모멘텀을 비교하여 기준자산보다 좋으면 ticker선택, 아니면 안전자산(dualticker[1])을 선택
 def getDualMomentumAsset(assets, months, rank, tickers, dualtickers) :
     momentum = assets.pct_change(periods=months)
-    print('momentum head(20) \n', momentum.head(50))
-    print('momentum tail \n', momentum.tail())
+    #print('momentum head(20) \n', momentum.head(50))
+    #print('momentum tail \n', momentum.tail())
     
     mom_rank = momentum[tickers].rank(ascending=False,axis=1)
-    print('mom_rank head(20) \n', mom_rank.head(50))
-    print('mom_rank tail \n', mom_rank.tail)
+    #print('mom_rank head(20) \n', mom_rank.head(50))
+    #print('mom_rank tail \n', mom_rank.tail)
 
     selected = pd.DataFrame(columns = momentum.columns)
     for i in range(0, momentum.shape[0]):
         stat = pd.DataFrame(np.zeros((1,len(momentum.columns)) , dtype=int), columns = momentum.columns)
         #print('index:{}'.format(momentum.index[i]))
-        stat[dualtickers[0]] = False
-        stat[dualtickers[1]] = False
+        for j in range(0, len(tickers)) :
+            stat[tickers[j]] = False
+        for k in range(0, len(dualtickers)) :
+            stat[dualtickers[k]] = False
+
         for j in range(0, len(tickers)) :
             #print('mom_rank[{}]'.format(tickers[j]), mom_rank[tickers[j]].iloc[i])
             if mom_rank[tickers[j]].iloc[i] <= rank:
@@ -69,14 +72,11 @@ def getDualMomentumAsset(assets, months, rank, tickers, dualtickers) :
                 if momentum[tickers[j]].iloc[i] > momentum[dualtickers[0]].iloc[i]:
                     stat[tickers[j]] = True
                 else:
-                    stat[tickers[j]] = False
                     stat[dualtickers[1]] = True
-            else:
-                stat[tickers[j]] = False
         #print('stat :', stat)
         selected = pd.concat([selected, stat])
         
-    print('df_stat shape: ', selected.shape)
+    #print('df_stat shape: ', selected.shape)
     selected.index = momentum.index
     #selected = selected.loc[start_day:]       
     return selected
